f05c59944c81f8dbdc102a605ee0c313a5567bb0
Alvarrir <khng.chenshuo@dhs.sg>
2020-04-01 03:02:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request
+import datetime
+import re
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+  result1 = 0
+  with open("INFO.txt") as file:
+    for line in file:
+      if "y1" in line:
+        result1 += 1
+  result2 = 0
+  with open("INFO.txt") as file:
+    for line in file:
+      if "y2" in line:
+        result2 += 1
+  currenttime = datetime.datetime.now()
+  if currenttime == "00:00:00.00000":
+    f = open('file.txt', 'r+')
+    f.truncate(0)
+  if request.method == 'GET':
+    return render_template('home.html', result1=result1, result2=result2)
+
+@app.route('/attendance', methods=['GET', 'POST'])
+def index():
+  if request.method == 'GET':
+    return render_template('attendance.html')
+  else:
+    todaysdate = datetime.date.today()
+    currentdate = todaysdate.isoformat()
+    name = request.form.get('name')
+    mobile = request.form.get('mobile')
+    breakfast = request.form.get('breakfast')
+    dinner = request.form.get('dinner')
+    pattern = re.compile("^[8-9][0-9]{7}$")
+    if pattern.match(mobile):
+      fout = open("INFO.txt", 'a')
+      fout.write(name + ',' + mobile + ',' + breakfast + ',' + dinner + ',' + currentdate + '\n')
+      fout.close()  
+      return render_template("attendance.html")
+    else:
+      return render_template("secret.html")
+
+@app.route('/secret', methods=['GET', 'POST'])
+def secret():
+  return render_template("secret.html")
+
+app.run(host='0.0.0.0', port=8080)
