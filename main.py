02b6ba3507956cfaa1b6e77063cae10c8043a386
Alvarrir <khng.chenshuo@dhs.sg>
2020-04-01 05:54:37 +0000

    dhostel

--- main.py
+++ main.py
@@ -44,12 +44,8 @@ def index():
     else:
       return render_template("secret.html")
 
-@app.route('/secret', methods=['GET', 'POST'])
-def secret():
-  return render_template("secret.html")
-
 @app.route('/delete', methods=['GET', 'POST'])
-def delete():
+def secret():
   return render_template("delete.html")
 
 app.run(host='0.0.0.0', port=8080)
